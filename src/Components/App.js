29788043bf3f8f86f5b9d1d4e03ad7751e932030
Sam Knowles <samuelkcknowles@gmail.com>
2018-10-17 19:18:49 -0600

    finished three by three grid

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThreeByThree from'./ThreeByThree';
+import '../Styles/ThreeByThree.css';
+
+function App() {
+    return (
+        <div>
+            <ThreeByThree></ThreeByThree>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BroswerRouter as Router } from 'react-router-dom';
+
+import App from './Components/App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
