5c0e6a6297bbf993067fadc70a80ea57b3a70ca5
Sam Knowles <samuelkcknowles@gmail.com>
2018-10-17 19:41:51 -0600

    added grid column and layout components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ThreeByThree from'./ThreeByThree';
+import GridColumns from './GridColumns'
+import GridLayout from './GridLayout'
 import '../Styles/ThreeByThree.css';
+import '../Styles/GridColumns.css';
+import '../Styles/GridLayout.css';
 
 function App() {
     return (
         <div>
-            <ThreeByThree></ThreeByThree>
-        </div>
+            <ThreeByThree />
+            <GridColumns />
+            <GridLayout />
+        </div> 
     )
 }
 
