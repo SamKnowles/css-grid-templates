93352874ef7ecb7e27ac46c48470f1422f1868a7
Sam Knowles <samuelkcknowles@gmail.com>
2018-10-17 20:31:12 -0600

    Finished basic Grid Template

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ThreeByThree from'./ThreeByThree';
-import GridColumns from './GridColumns'
-import GridLayout from './GridLayout'
-import '../Styles/ThreeByThree.css';
+import GridElements from'./GridElements';
+import GridColumns from './GridColumns';
+import GridLayout from './GridLayout';
+import GridRows from './GridRows';
+import GridGaps from './GridGaps';
+import GridGapPlus from './GridGapPlus';
+import GridLines from './GridLines';
+import GridLinesPlus from './GridLinesPlus';
+import '../Styles/GridLines.css';
+import '../Styles/GridLinesPlus.css';
+import '../Styles/GridElements.css';
 import '../Styles/GridColumns.css';
 import '../Styles/GridLayout.css';
+import '../Styles/GridRows.css';
+import '../Styles/GridGaps.css';
+import '../Styles/GridGapPlus.css';
 
 function App() {
     return (
         <div>
-            <ThreeByThree />
-            <GridColumns />
             <GridLayout />
+            <GridElements />
+            <GridColumns />    
+            <GridRows />
+            <GridGaps />
+            <GridGapPlus />
+            <GridLines />
+            <GridLinesPlus />
         </div> 
     )
 }
